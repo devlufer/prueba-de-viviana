ceb4edeeb33ce14f820a7ad2693ad2ab58aabf6b
VivianaChoque-127 <vivianachoque127@gmail.com>
2021-05-19 10:43:05 -0400

    cambios en textos

--- suma.php
+++ suma.php
@@ -7,13 +7,14 @@ echo $suma;
 <html>
 <body>
     <h1>prueba de viviana</h1>
-    <h1>suma de numeros</h1>asdf
-<form action="suma.php" method="post">
-    <label for="">ingrese el primer numero</label><br>
+    <h1>suma de numeros</h1>
+   <form action="suma.php" method="post">
+    <label for=""><FONT COLOR="blue">ingrese el primer numero   </FONT></label><br>
     <input type="text" name="uno"><br>
-    <label for="">ingrese el segundo numero</label><br>
+    <label for=""> <FONT COLOR="blue"> ingrese el segundo numero</FONT></label><br>
     <input type="text" name="dos"><br>
     <input type="submit" value="sumar">
+    <input type="submit" value="restar">
 </form>
 </body>
 </html>
